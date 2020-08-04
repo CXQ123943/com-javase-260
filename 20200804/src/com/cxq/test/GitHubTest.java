59a82ab467cf0124b668de8269bda8be2e7e5993
cxq <1249560518@qq.com>
2020-08-04 15:41:39 +0800

    新增Test测试

---
+++ 20200804/src/com/cxq/test/GitHubTest.java
@@ -0,0 +1,11 @@
+package com.cxq.test;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+public class GitHubTest {
+    public static void main(String[] args){
+        System.out.println("dkaspf");
+    }
+}
