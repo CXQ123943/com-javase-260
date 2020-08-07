5d1175a0d5bef08585ae8bc1b9b29daa5dfdcfcf
cxq <1249560518@qq.com>
2020-08-07 12:02:42 +0800

    对羊这个类写了另外两个方法：多次浅克隆和深克隆

--- 20200807/src/com/cxq/prototype/Sheep.java
+++ 20200807/src/com/cxq/prototype/Sheep.java
@@ -1,12 +1,13 @@
 package com.cxq.prototype;
 
+import java.io.*;
 import java.util.Date;
 
 /**
  * @author CXQ
  * @version 1.0
  */
-public class Sheep implements Cloneable{
+public class Sheep implements Cloneable, Serializable{
     private String name;
     private Date birth;
 
@@ -34,5 +35,47 @@ public class Sheep implements Cloneable{
             e.printStackTrace();
         }
             return result;
+    }
+
+
+    public Sheep multipleShallowClone(Sheep sheep) {
+        Sheep result = null;
+        try {
+            result = (Sheep) super.clone();
+            Date date = (Date)sheep.birth.clone();
+            sheep.setBirth(date);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public Sheep deepClone(Sheep sheep) {
+        Sheep result = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(baos);
+            oos.writeObject(sheep);
+            oos.flush();
+            ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+            ois = new ObjectInputStream(bais);
+            result = (Sheep) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
