87f463669e9fab50b1207fbb71018eeb3727f116
cxq <1249560518@qq.com>
2020-08-26 10:37:40 +0800

    SocketChannel：服务端与客户端的测试（添加注释）

--- 20200826/src/com/cxq/nio/BlockSocketServer.java
+++ 20200826/src/com/cxq/nio/BlockSocketServer.java
@@ -13,18 +13,28 @@ import java.nio.channels.SocketChannel;
 public class BlockSocketServer {
     public static void main(String[] args) throws IOException {
         int port = 9999;
+        //开启一个ServerSocketChannel通道
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //创建一个socket地址：使用“new InetSocketAddress()”来构造
         InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+        //ServerSocketChannel通道绑定一个socket地址
         serverSocketChannel.bind(inetSocketAddress);
+
         System.out.println("ready to accept data...");
+        //服务端通道等待接收数据
         SocketChannel accept = serverSocketChannel.accept();
+        //创建一个缓冲区
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        //接收的数据写到缓冲区里
         while (accept.read(byteBuffer) != -1) {
+            //指针pos复位（归0）
             byteBuffer.flip();
             for (int i = 0, j = byteBuffer.limit(); i < j; i++) {
+                //获取缓冲区的数据并输出
                 System.out.print((char) byteBuffer.get());
             }
             System.out.println();
+            //pos limit复位，为下一个数据的读和获取做准备
             byteBuffer.clear();
         }
         accept.close();
