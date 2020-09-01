68bc8a8d644b0cce63a9da3699ca949578bba383
cxq <1249560518@qq.com>
2020-09-01 12:14:52 +0800

    ThreadLocal测试

---
+++ 20200901/src/com/cxq/sync/ThreadLocalTest.java
@@ -0,0 +1,37 @@
+package com.cxq.sync;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+public class ThreadLocalTest {
+    private static class Person {
+        private String name;
+    }
+    private static ThreadLocal<Person> personThreadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1L);
+                personThreadLocal.set(new Person());
+                System.out.println("1秒钟后Person设置完成...");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2L);
+                System.out.println(personThreadLocal.get() == null ? "2秒钟后获取失败.." : "2秒钟后获取成功");
+                personThreadLocal.remove();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
