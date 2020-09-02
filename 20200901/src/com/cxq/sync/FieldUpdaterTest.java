91c86aa68bf28d3f7ee6051fbef60a93a8946e2b
cxq <1249560518@qq.com>
2020-09-02 08:52:46 +0800

    偏向锁测试

--- 20200901/src/com/cxq/sync/FieldUpdaterTest.java
+++ 20200901/src/com/cxq/sync/FieldUpdaterTest.java
@@ -28,13 +28,13 @@ public class FieldUpdaterTest {
 
     @Test
     public void atomicLongFieldUpdater() {
-        Student student = new Student(1L,"zhao-si",15);
+        Student student = new Student(1L, "zhao-si", 15);
         AtomicLongFieldUpdater<Student> updater = AtomicLongFieldUpdater.newUpdater(Student.class, "id");
 
         updater.get(student);
         System.out.println("id from student：" + student.getId());
 
-        updater.compareAndSet(student,1L,5L);
+        updater.compareAndSet(student, 1L, 5L);
         System.out.println("update id from student to 5L：" + student.getId());
 
         updater.incrementAndGet(student);
@@ -55,13 +55,13 @@ public class FieldUpdaterTest {
 
     @Test
     public void atomicIntegerFieldUpdater() {
-        Student student = new Student(1L,"zhao-si",10);
-        AtomicIntegerFieldUpdater<Student> updater = AtomicIntegerFieldUpdater.newUpdater(Student.class,"age");
+        Student student = new Student(1L, "zhao-si", 10);
+        AtomicIntegerFieldUpdater<Student> updater = AtomicIntegerFieldUpdater.newUpdater(Student.class, "age");
 
         updater.get(student);
         System.out.println("age from student：" + student.getAge());
 
-        updater.compareAndSet(student,10,20);
+        updater.compareAndSet(student, 10, 20);
         System.out.println("update age from student to 20：" + student.getAge());
 
         updater.incrementAndGet(student);
@@ -82,33 +82,33 @@ public class FieldUpdaterTest {
 
     @Test
     public void atomicReferenceFieldUpdater() {
-        Student student = new Student(1L,"wangdefa",10);
+        Student student = new Student(1L, "wangdefa", 10);
         AtomicReferenceFieldUpdater<Student, String> updater = AtomicReferenceFieldUpdater.newUpdater(Student.class, String.class, "name");
 
         updater.get(student);
         System.out.println("name from student：" + student.getName());
 
-        updater.compareAndSet(student,"wangdefa","cxq");
+        updater.compareAndSet(student, "wangdefa", "cxq");
         System.out.println("update name from student to cxq：" + student.getName());
 
-        updater.lazySet(student,"WTF");
+        updater.lazySet(student, "WTF");
         System.out.println("newName：" + student.getName());
 
-        updater.getAndSet(student,"OMG");
+        updater.getAndSet(student, "OMG");
         System.out.println("newName：" + student.getName());
 
         updater.getAndAccumulate(student, "LALALA", new BinaryOperator<String>() {
             @Override
             public String apply(String s, String s2) {
-                return (s +s2);
+                return (s + s2);
             }
         });
         System.out.println("newName：" + student.getName());
 
-        updater.set(student,"aaa");
+        updater.set(student, "aaa");
         System.out.println("newName：" + student.getName());
 
-        updater.weakCompareAndSet(student,"bbb","ccc");
+        updater.weakCompareAndSet(student, "bbb", "ccc");
         System.out.println("newName：" + student.getName());
     }
 }
