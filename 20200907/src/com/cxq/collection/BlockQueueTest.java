59bf33e67ef1c1f95a2e368c6adc21fdb7de6fba
cxq <1249560518@qq.com>
2020-09-07 15:41:00 +0800

    同步容器Queue

--- 20200907/src/com/cxq/collection/BlockQueueTest.java
+++ 20200907/src/com/cxq/collection/BlockQueueTest.java
@@ -6,10 +6,7 @@ import org.junit.Test;
 
 import java.util.Queue;
 import java.util.Random;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author CXQ
@@ -25,8 +22,7 @@ public class BlockQueueTest {
     }
 
     @Test
-    public void linkedBlockingDeque() {
-
+    public void linkedBlockingQueue() {
         BlockingQueue<Integer> list = new LinkedBlockingDeque<>();
         new Thread(() -> {
             try {
@@ -51,6 +47,80 @@ public class BlockQueueTest {
                     }
                 }
             }).start();
+        }
+    }
+
+    @Test
+    public void linkedBlockingDeque() {
+        LinkedBlockingDeque<String> list = new LinkedBlockingDeque<>();
+        list.addFirst("zhao-si");
+        list.addLast("liu-neng");
+        System.out.println(list.peekFirst());
+        System.out.println(list.peekLast());
+        System.out.println(list);
+    }
+
+    @SneakyThrows
+    @Test
+    public void arrayBlockingQueue() {
+        BlockingQueue<Integer> list = new ArrayBlockingQueue<>(10);
+        for (int i = 0, j = 10; i < j; i++) {
+            list.put(i);
+        }
+        System.out.println(list.size());
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2L);
+                System.out.println(list.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "YYY").start();
+
+        // blocking and wait for consumer
+        list.put(250);
+        System.out.println("over...");
+    }
+
+    private class MyTask implements Delayed {
+
+        private String taskName;
+        private Long timestamp;
+
+        MyTask(String taskName, Long timestamp) {
+            this.taskName = taskName;
+            this.timestamp = timestamp;
+        }
+
+        /**
+         * 计算延迟
+         * */
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return unit.convert(timestamp - System.currentTimeMillis(), TimeUnit.SECONDS);
+        }
+
+        @Override
+        public int compareTo(Delayed o) {
+            return (int) (getDelay(TimeUnit.MILLISECONDS) - o.getDelay(TimeUnit.MILLISECONDS));
+        }
+
+        @Override
+        public String toString() {
+            return taskName + ": " + timestamp;
+        }
+    }
+
+    @SneakyThrows
+    @Test
+    public void delayQueue() {
+        DelayQueue<MyTask> list = new DelayQueue<>();
+        long currentTime = System.currentTimeMillis();
+        list.put(new MyTask("task01", currentTime + 3));
+        list.put(new MyTask("task02", currentTime + 1));
+        list.put(new MyTask("task03", currentTime + 2));
+        for (int i = 0, j = list.size(); i < j; i++) {
+            System.out.println(list.take());
         }
     }
 
