e8ab0d168597f26d9088b14f8f67dcae3bb18652
cxq <1249560518@qq.com>
2020-09-08 14:45:15 +0800

    线程池：ThreadPoolExecutor测试

---
+++ 20200908/src/com/cxq/pool/ThreadPoolExecutorTest.java
@@ -0,0 +1,44 @@
+package com.cxq.pool;
+
+import lombok.SneakyThrows;
+import org.junit.After;
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+/**
+ * @author CXQ
+ * @version 1.0
+ */
+public class ThreadPoolExecutorTest {
+
+    @SneakyThrows
+    @Test
+    public void threadPoolExecutor() {
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+                2, 4, 3, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
+
+        threadPool.execute(() -> {
+            System.out.println("runnable...");
+        });
+
+        TimeUnit.SECONDS.sleep(2L);
+
+        Future<Integer> future = threadPool.submit(() -> {
+            System.out.println("callable...");
+            return 100;
+        });
+        System.out.println(future.get());
+    }
+
+
+    @SneakyThrows
+    @After
+    public void after() {
+        System.out.println(System.in.read());
+    }
+}
